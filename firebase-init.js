dcddc40b39ee9e9f5b0426d9cafcb226de17cc8d
jose carrizo <santinodionel02@gmail.com>
2026-01-07 22:56:38 -0300

    first commit

---
+++ firebase-init.js
@@ -0,0 +1,22 @@
+// ==============================
+//  Firebase INIT — RuralControl
+// ==============================
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// --- CONFIG ---
+const firebaseConfig = {
+  apiKey: "AIzaSyCRpSuQ3nSVvThu246TT0ai-G4OB9_yn_g",
+  authDomain: "red-rural.firebaseapp.com",
+  projectId: "red-rural",
+  storageBucket: "red-rural.firebasestorage.app",
+  messagingSenderId: "30695077122",
+  appId: "1:30695077122:web:2c700973abeac5914dbe1b"
+};
+
+// --- INICIALIZACIÓN ---
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
